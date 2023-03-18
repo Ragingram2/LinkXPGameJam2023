22e65b96a7d53b125b2a9d9d3cfc89fcee09c12a
Jan Benko <jan.benko777@gmail.com>
2023-03-18 19:32:53 +0100

    Enemy testing
    Collisions, sphere collider and box collider

--- Assets/Scripts/EnemyFolder/Enemy.cs
+++ Assets/Scripts/EnemyFolder/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -25,17 +26,25 @@ public class Enemy : MonoBehaviour
 
     //public Rigidbody body;
     public Transform target;
+    Transform finalTarget;
     public NavMeshAgent agent;
     public CapsuleCollider capsuleCollider;
-    
 
-    //Enemy target
-    //GameObject target;
+    public BoxCollider targetBoxCollider; //should be farm or wall collider
+    public SphereCollider enemySphereCollider;
+
+    float timerLenght = 5.0f;
+    float timeGoing;
+    bool startedCountdown = false;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         capsuleCollider = GetComponent<CapsuleCollider>();
+        enemySphereCollider = GetComponent<SphereCollider>();
+
+        targetBoxCollider = target.GetComponent<BoxCollider>();
+
 
         enemyName = enemyData.name;
         enemyDescription = enemyData.description;
@@ -44,12 +53,15 @@ public class Enemy : MonoBehaviour
         enemyCurrentHealth = enemyMaxHealth;
 
         enemySpeed = enemyData.speed;
+        agent.speed = enemySpeed; 
 
         enemyDamageDeal = enemyData.damageDeal;
 
         enemyAttackTime = enemyData.attackTime;
 
         enemyRadiusMultiply = enemyData.radiusMultiply;
+
+        finalTarget = target;
 
         DebugLog();
     }
@@ -59,40 +71,84 @@ public class Enemy : MonoBehaviour
         EnemyMove();
     }
 
-
-    void OnTriggerEnter(Collider other)
+    private void LateUpdate()
     {
-        //agent.SetDestination(transform.position);
-        Debug.Log($"Enemy type {enemyName} is going here: " + transform.position);
+        if (startedCountdown)
+        {
+            timeGoing += Time.deltaTime;
+            Debug.Log(timeGoing);
+            if (timeGoing > timerLenght)
+            {
+                timeGoing = 0;
+                startedCountdown = false;
+                SwitchEnemyTarget(finalTarget);
+                Debug.Log("Timer expired");
+                //agent.speed = enemySpeed;
+            }
+        }
     }
+
+    
+
+    void OnTriggerEnter(Collider collider)
+    {
+        
+        if (collider.gameObject.name == "AttackTarget")
+        {
+            SwitchEnemyTarget(collider.gameObject.transform);
+            startedCountdown = true;
+            Debug.Log(startedCountdown);
+            Debug.Log($"Ran into this thing: {collider.gameObject.name}");
+        }
+
+        if (collider.gameObject.name == target.name)
+        {           
+            agent.speed = 0f;
+            EnemyAttack();
+            Debug.Log($"Enemy type {enemyName} speed is: " + agent.speed);
+            Debug.Log($"Collider name: {collider.name}");
+        }
+    }
+
+    // Dictionary that makes lists based on priority numbers
+  
 
     void DebugLog()
     {
         Debug.Log( $"Enemy type {enemyName}: " + enemyDamageDeal);
     }
 
-    void EnemyMove()
+    public void SwitchEnemyTarget(Transform pTarget)
+    {
+        Debug.Log($"Should've switched target to {pTarget.name}");
+        target = pTarget;
+        agent.speed = enemySpeed;
+
+
+        //target = FindObjectOfType < house or wall or whatever > ();
+        //priority: highest damage
+        //Needs sphere collider for triggers
+    }
+
+
+    protected virtual void EnemyMove()
     {
         if (target != null)
         {
-            agent.speed = enemySpeed;
+
             var pos = target.position;
             pos.y = 0;
             agent.SetDestination(pos);
         }
 
-        if (agent.remainingDistance < capsuleCollider.radius * enemyRadiusMultiply)
-        {
-            agent.speed = 0f;
-        }
         //^This will be replaced by farm house and the farm house radius
     }
-
-
-    void SwitchEnemyTarget()
+    protected virtual void EnemyAttack()
     {
-        //target = FindObjectOfType < house or wall or whatever > ();
-        //if wall is in front, switch the attack target to wall, else go base
-    }
-
+        //If sphere collider is trigger colliding with wall or base - attack
+        Debug.Log("Attacked target");
+    }    
 }
+
+
+//If the enemy is colliding(it's box collision) with anything that's a tower, it deals damage
