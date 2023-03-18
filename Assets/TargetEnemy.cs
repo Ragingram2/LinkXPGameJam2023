6c5cbbc8a981aff1596548412defc85cafc79f26
Rowan Ramsey <blazinram@gmail.com>
2023-03-18 05:41:33 +0100

    simple tower targeting

--- Assets/TargetEnemy.cs
+++ Assets/TargetEnemy.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TargetEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private List<GameObject> m_targets = new List<GameObject>();
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(GetClosestEnemyPos());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Enemy"))
+        {
+            m_targets.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (m_targets.Contains(other.gameObject))
+        {
+            m_targets.Remove(other.gameObject);
+        }
+    }
+
+    Vector3 GetClosestEnemyPos()
+    {
+        float maxDistance = Mathf.Infinity;
+        GameObject closest = null;
+        foreach (var go in m_targets)
+        {
+            var dist = Vector3.Distance(transform.position, go.transform.position);
+            if ( dist < maxDistance)
+            {
+                maxDistance = dist;
+                closest = go;
+            }
+        }
+        var closestPos = closest.transform.position;
+        closestPos.y = transform.position.y;
+        return closest != null ? closestPos : transform.forward;
     }
 }
