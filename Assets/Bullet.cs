ee93c9ecc9a510a71365e9bfbf2c25618469e0aa
Rowan Ramsey <blazinram@gmail.com>
2023-03-19 02:03:14 +0100

    mortars fucking suck

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        Destroy(gameObject);
-    }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Destroy(gameObject);
+    //}
 }
--- Assets/Scripts/Towers/SpiderTower.cs
+++ Assets/Scripts/Towers/SpiderTower.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class SpiderTower : Tower
 {
     float launchSpeed;
+    Vector3 launchVel;
     void OnValidate()
     {
         float x = m_data.m_range + 0.25001f;
         float y = -transform.position.y;
-        launchSpeed = Mathf.Sqrt(9.81f * (y + Mathf.Sqrt(x * x + y * y)));
+        launchSpeed = Mathf.Sqrt((y + Mathf.Sqrt(x * x + y * y)));
     }
-
 
     public override void TowerAttack()
     {
@@ -19,11 +19,21 @@ public class SpiderTower : Tower
         transform.LookAt(m_curretTarget.transform);
         Debug.DrawLine(transform.position, targetPos);
 
+        RaycastHit hit;
+        if (Physics.Raycast(m_gunTip.position, transform.forward, out hit, Mathf.Infinity, m_targetMask))
+        {
+            var name = LayerMask.LayerToName(hit.transform.gameObject.layer);
+            if (!name.Equals("Target"))
+            {
+                return;
+            }
+        }
+
         Vector2 dir;
-        dir.x = targetPos.x - targetPos.x;
-        dir.y = targetPos.z - targetPos.z;
+        dir.x = targetPos.x - m_gunTip.position.x;
+        dir.y = targetPos.z - m_gunTip.position.z;
         float x = dir.magnitude;
-        float y = -m_curretTarget.transform.position.y;
+        float y = -m_gunTip.position.y;
         dir /= x;
 
         float g = 9.81f;
@@ -35,8 +45,19 @@ public class SpiderTower : Tower
         float cosTheta = Mathf.Cos(Mathf.Atan(tanTheta));
         float sinTheta = cosTheta * tanTheta;
 
-        var go = Instantiate(m_bullet,m_gunTip.transform.position,Quaternion.identity);
-        go.GetComponent<Rigidbody>().AddForce(new Vector3(s * cosTheta * dir.x, s * sinTheta, s * cosTheta * dir.y));
+        launchVel = new Vector3(s * cosTheta * dir.x, s * sinTheta, s * cosTheta * dir.y) * launchSpeed * 1000f;
+        Debug.Log(launchVel);
+        if (m_canAttack)
+        {
+            var go = Instantiate(m_bullet, m_gunTip.transform.position + transform.forward, Quaternion.identity);
+            go.GetComponent<Rigidbody>().AddForce(launchVel, ForceMode.Impulse);
+            StartCoroutine(ShotCooldown(m_data.m_fireRate));
+        }
+    }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, launchVel);
     }
 }
